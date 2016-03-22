98c320c328c3ffccef6bb7a7ed84de5a9dfdd54a
J. Oliva <jesus@epiclabs.io>
2016-03-22 15:54:49 +0100

    Add gulp support

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp'),
+    spawn = require('child_process').spawn,
+    node;
+
+gulp.task('server', function() {
+  if (node) {
+      node.kill()
+  }
+  
+  node = spawn('node', ['app.js'], {stdio: 'inherit'})
+  node.on('close', function (code) {
+    if (code === 8) {
+      gulp.log('Error detected, waiting for changes...');
+    }
+  });
+});
+
+gulp.task('default', ["server"]);
+
+// clean up if an error goes unhandled.
+process.on('exit', function() {
+    if (node) {
+        node.kill()
+    }
+});
