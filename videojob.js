4ea2b32ef63a77d1489236c50d9a16bd8b0005eb
J. Oliva <jesus@epiclabs.io>
2016-05-11 19:07:24 +0200

    Added user agent

--- videojob.js
+++ videojob.js
@@ -210,8 +210,8 @@ function buildFfmpegCommand(job) {
              log("Generation of HLS output files started", job);
              
              if (job.callbackUrl !== undefined && job.callbackUrl.length > 0) {
-                request({uri: job.callbackUrl, method: "POST", json: {"id": job.id, "upcloseStreamUrl": job.upcloseStreamUrl}}, function(error, response, body) {
-                    log("Calling callback to notify stream started: " + job.callbackUrl + ", response: " + response + ", body: " + body, job);
+                request({uri: job.callbackUrl, headers: {"User-agent": "HLSProxy/0.1"}, method: "POST", json: {"id": job.id, "upcloseStreamUrl": job.upcloseStreamUrl}}, function(error, response, body) {
+                    log("Calling callback to notify stream started: " + job.callbackUrl, job);
                     if (error || response.statusCode != 200) {
                         job.signalError("Error calling callback: " + error + ", body: " + body);
                     }
