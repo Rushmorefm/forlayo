1b26fceffe626fb20c5a7e1c27bb7d04f982e799
J. Oliva <jesus@epiclabs.io>
2016-05-18 14:27:07 +0200

    Change segment length

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ class Config {
         }   
         
         // Parse HLS segment size (in seconds)
-        this.OUTPUT_VIDEO_HLS_SEGMENT_SIZE = utils.readIntConfigParam(process.env.hlsSegmentSize, 10, 4, 20);
+        this.OUTPUT_VIDEO_HLS_SEGMENT_SIZE = utils.readIntConfigParam(process.env.hlsSegmentSize, 4, 2, 10);
 
         // Parse max number of segments. 0 means no limit 
         this.OUTPUT_VIDEO_MAX_SEGMENTS = utils.readIntConfigParam(process.env.hlsMaxSegments, 0, 0, undefined);
